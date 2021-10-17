6fb30fb422402c09c74347bcdcafb8255468a7db
Shaun <thedeadsent@gmail.com>
2021-10-17 19:27:26 -0400

    final commit

--- api/posts.js
+++ api/posts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const postsRouter = express.Router();
 const {requireUser} = require('./utils')
-const { getAllPosts } = require("../db");
+const { getAllPosts, createPost, getPostById, updatePost } = require("../db");
 
 postsRouter.use((req, res, next) => {
   console.log("A requests is being made to /posts");
@@ -9,37 +9,113 @@ postsRouter.use((req, res, next) => {
   next();
 });
 
-postsRouter.get("/", async (req, res) => {
+postsRouter.get('/', async (req, res) => {
   try {
-    const posts = await getAllPosts();
-    res.send({ posts });
-  } catch (error) {
-    throw error;
+    const allPosts = await getAllPosts();
+
+    const posts = allPosts.filter(post => {
+      // the post is active, doesn't matter who it belongs to
+      if (post.active) {
+        return true;
+      }
+    
+      // the post is not active, but it belogs to the current user
+      if (req.user && post.author.id === req.user.id) {
+        return true;
+      }
+    
+      // none of the above are true
+      return false;
+    });
+
+    res.send({
+      posts
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
 
-const { requireUser } = require('./utils');
-
 postsRouter.post('/', requireUser, async (req, res, next) => {
   const { title, content, tags = "" } = req.body;
+  const authorId = req.user.id
 
   const tagArr = tags.trim().split(/\s+/)
-  const postData = {};
+  const postData = {authorId, title, content, tags};
 
-  // only send the tags if there are some to send
   if (tagArr.length) {
     postData.tags = tagArr;
   }
 
-  try {
-    
-    // add authorId, title, content to postData object
-    // const post = await createPost(postData);
-    // this will create the post and the tags for us
-    // if the post comes back, res.send({ post });
-    // otherwise, next an appropriate error object 
+  try {     
+    const post = await createPost(postData)
+
+    if(post){
+      res.send({post})
+    } else{
+      next({name: "IncorrectPostError", message: "Missing Content From Post"})
+    } 
   } catch ({ name, message }) {
     next({ name, message });
+  }
+});
+
+postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
+  const { postId } = req.params;
+  const { title, content, tags } = req.body;
+
+  const updateFields = {};
+
+  if (tags && tags.length > 0) {
+    updateFields.tags = tags.trim().split(/\s+/);
+  }
+
+  if (title) {
+    updateFields.title = title;
+  }
+
+  if (content) {
+    updateFields.content = content;
+  }
+
+  try {
+    const originalPost = await getPostById(postId);
+
+    if (originalPost.author.id === req.user.id) {
+      const updatedPost = await updatePost(postId, updateFields);
+      res.send({ post: updatedPost })
+    } else {
+      next({
+        name: 'UnauthorizedUserError',
+        message: 'You cannot update a post that is not yours'
+      })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+  try {
+    const post = await getPostById(req.params.postId);
+
+    if (post && post.author.id === req.user.id) {
+      const updatedPost = await updatePost(post.id, { active: false });
+
+      res.send({ post: updatedPost });
+    } else {
+      // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+      next(post ? { 
+        name: "UnauthorizedUserError",
+        message: "You cannot delete a post which is not yours"
+      } : {
+        name: "PostNotFoundError",
+        message: "That post does not exist"
+      });
+    }
+
+  } catch ({ name, message }) {
+    next({ name, message })
   }
 });
 
